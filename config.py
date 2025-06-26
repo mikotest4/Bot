b54beeaef35b222665e19cec7d177b4d4362e80b
mikotest4 <fakealok756@gmail.com>
2025-06-26 22:02:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,12 +3,12 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = list(map(int, getenv("OWNER_ID", "7782401729").split()))
+API_ID = int(getenv("API_ID", "27704224"))
+API_HASH = getenv("API_HASH", "c2e33826d757fe113bc154fcfabc987d")
+BOT_TOKEN = getenv("BOT_TOKEN", "7540338860:AAG2fB5W-5EBHnDRX43gcl9abnwCDyw4Bng")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "7970350353").split()))
 MONGO_DB = getenv("MONGO_DB", "mongodb+srv://koxiher824:uhWjdKAzmfAHcagy@cluster0.lg66r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-LOG_GROUP = getenv("LOG_GROUP", "-1002596328286")
-CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002413032052"))
-FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "0"))
+LOG_GROUP = getenv("LOG_GROUP", "-1002669902570")
+CHANNEL_ID = int(getenv("CHANNEL_ID", "-1002669902570"))
+FREEMIUM_LIMIT = int(getenv("FREEMIUM_LIMIT", "100"))
 PREMIUM_LIMIT = int(getenv("PREMIUM_LIMIT", "5000"))
