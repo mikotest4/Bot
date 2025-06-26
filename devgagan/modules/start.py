4738d03bd4d5975c8fff7b1cf30a6bc60380c602
mikotest4 <fakealok756@gmail.com>
2025-06-27 00:01:56 +0530

    Update start.py

--- devgagan/modules/start.py
+++ devgagan/modules/start.py
@@ -9,8 +9,8 @@ from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardBu
 
 buttons = InlineKeyboardMarkup(
     [
-        [InlineKeyboardButton("Join Channel", url="https://t.me/rajputserver")],
-        [InlineKeyboardButton("Buy Premium", url="tg://settings")]
+        [InlineKeyboardButton("Join Channel", url="https://t.me/+8iM2xyeUtGs4ZGYy")],
+        [InlineKeyboardButton("Buy Premium", url="https://t.me/Yae_X_Miko")]
     ]
 )
 
@@ -19,6 +19,6 @@ async def start(_, message):
     join = await subscribe(_, message)
     if join == 1:
         return
-    await message.reply_photo(photo="https://t.me/rajputserver/7",
+    await message.reply_photo(photo="https://graph.org/file/255a7bf3992c1bfb4b78a-03d5d005ec6812a81d.jpg",
                               caption=script.START_TXT.format(message.from_user.mention), 
                               reply_markup=buttons)
